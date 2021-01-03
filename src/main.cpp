191fa7bf62466b7cdef6d44cfe8cdff0510440f0
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-01-04 00:23:40 +0200

    exporting field data from femocs into vtk grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
-#include "AtomReader.h"
-#include "Config.h"
 #include "Femocs.h"
 #include "GeneralProject.h"
+#include "Globals.h"
 #include "Macros.h"
-#include "TetgenMesh.h"
-#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
 #include <vtkCellType.h>
+#include <vtkDoubleArray.h>
 #include <vtkMeta.h>
+#include <vtkPointData.h>
 #include <vtkUnstructuredGrid.h>
 #include <vtkXMLUnstructuredGridWriter.h>
 
@@ -25,15 +24,22 @@ void print_progress(const string &message, const bool contition) {
     cout << "failed" << endl;
 }
 
+void print_field(const string label, const int n_nodes, double *field_data) {
+  for (int i = 0; i < n_nodes; i++) {
+    if (field_data[i] == 0)
+      continue;
+    cout << label << "[" << i << "] : " << field_data[i] << endl;
+  }
+}
+
 int main() {
   string filename = "in/md.in";
 
   femocs::Femocs project(filename);
 
-  // double output_data[n_atoms * 3] = {0};
   int success = 0;
-  int n_iterations = 1;
-  bool add_rnd_noise = false;
+  int n_iterations = 100;
+  bool add_rnd_noise = true;
 
   for (int iter_i = 1; iter_i <= n_iterations; ++iter_i) {
     if (n_iterations > 1)
@@ -47,19 +53,43 @@ int main() {
     const double *nodes = NULL;
     const int n_coordinates = 3;
     const int n_nodes = project.export_data(&nodes, "nodes");
-    for (int i = 0; i < n_nodes; i++) {
-      int I = n_coordinates * i;
-      printf("%.3f %.3f %.3f\n", nodes[I], nodes[I + 1], nodes[I + 2]);
-    }
+    // for (int i = 0; i < n_nodes; i++) {
+    //   int I = n_coordinates * i;
+    //   printf("%.3f %.3f %.3f\n", nodes[I], nodes[I + 1], nodes[I + 2]);
+    // }
 
     const int *cells = NULL;
     const int n_nodes_per_cell = 4;
     const int n_cells = project.export_data(&cells, "quadrangles");
-    for (int i = 0; i < n_cells; i++) {
-      int I = n_nodes_per_cell * i;
-      printf("%d %d %d %d\n", cells[I], cells[I + 1], cells[I + 2],
-             cells[I + 3]);
-    }
+    // for (int i = 0; i < n_cells; i++) {
+    //   int I = n_nodes_per_cell * i;
+    //   printf("%d %d %d %d\n", cells[I], cells[I + 1], cells[I + 2],
+    //          cells[I + 3]);
+    // }
+
+    // Field data
+
+    double elfield_data[n_nodes] = {0};
+    double temperature_data[n_nodes] = {0};
+    project.export_data(elfield_data, n_nodes, "elfield");
+    project.export_data(temperature_data, n_nodes, "temperature");
+
+    vtkSmartPointer<vtkDoubleArray> elfield =
+        vtkSmartPointer<vtkDoubleArray>::New();
+    elfield->SetName("elfield");
+    for (int i = 0; i < n_nodes; i++)
+      elfield->InsertNextValue(elfield_data[i]);
+
+    vtkSmartPointer<vtkDoubleArray> temperature =
+        vtkSmartPointer<vtkDoubleArray>::New();
+    temperature->SetName("temperature");
+    for (int i = 0; i < n_nodes; i++)
+      temperature->InsertNextValue(temperature_data[i]);
+
+    vtkSmartPointer<vtkPointData> pointFieldData =
+        vtkSmartPointer<vtkPointData>::New();
+    pointFieldData->AddArray(elfield);
+    pointFieldData->AddArray(temperature);
 
     // VTK mesh
 
@@ -72,18 +102,19 @@ int main() {
     for (int i = 0; i < n_nodes; i++)
       points->InsertPoint(i, coordinates[i]);
 
-    vtkIdType cellsIndecies[n_cells][n_nodes_per_cell];
+    vtkIdType cellsIndicies[n_cells][n_nodes_per_cell];
     for (int i = 0; i < n_cells; i++) {
       for (int j = 0; j < n_nodes_per_cell; j++)
-        cellsIndecies[i][j] = cells[n_nodes_per_cell * i + j];
+        cellsIndicies[i][j] = cells[n_nodes_per_cell * i + j];
     }
 
     vtkSmartPointer<vtkUnstructuredGrid> grid =
         vtkSmartPointer<vtkUnstructuredGrid>::New();
     grid->Allocate(n_cells);
     for (int i = 0; i < n_cells; i++)
-      grid->InsertNextCell(VTK_QUAD, n_nodes_per_cell, cellsIndecies[i]);
+      grid->InsertNextCell(VTK_QUAD, n_nodes_per_cell, cellsIndicies[i]);
     grid->SetPoints(points);
+    grid->SetFieldData(pointFieldData);
 
     vtkSmartPointer<vtkXMLUnstructuredGridWriter> writer =
         vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
@@ -91,20 +122,8 @@ int main() {
     writer->SetFileName(filename.c_str());
     writer->SetInputData(grid);
     writer->Write();
-
-    // Field data
-
-    // for (auto label : labels) {
-    //   cout << "exporting " << label << endl;
-    //   project.export_data(output_data, n_atoms, label);
-    //   for (int j = 0; j < n_atoms * 3; j++) {
-    //     cout << "\t" << label << "[" << j << "] : " << output_data[j] <<
-    //     endl;
-    //   }
-    // }
   }
 
   print_progress("\n> full run of Femocs", success == 0);
-
   return 0;
 }
